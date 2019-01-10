589af1b3b3ddf75213c2e69fca1401820b1b5a4c
Guillermo Bonafonte <guillermo.bonafontecriado@cognizant.com>
2019-01-10 10:53:56 +0100

    Implemented templates and partial templates in order to show recipes created on the fly.

--- app/controllers/RecipeController.java
+++ app/controllers/RecipeController.java
@@ -1,7 +1,12 @@
 package controllers;
 
-import play.mvc.*;
 import com.fasterxml.jackson.databind.JsonNode;
+import models.Ingredient;
+import models.Recipe;
+import play.mvc.*;
+import play.twirl.api.Content;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RecipeController extends Controller {
 
@@ -9,15 +14,27 @@ public class RecipeController extends Controller {
 
     public Result retrieveRecipe(Integer recipeId) {
         Result result;
+
+        Recipe recipe = new Recipe();
+        recipe.setId(1);
+        recipe.setName("Macarrones");
+        recipe.setImageURL("imagen");
+        recipe.setIngredients(new ArrayList<Ingredient>());
+
         if(request().accepts(Http.MimeTypes.JSON)){
-            result = ok("Retrieve JSON"+recipeId)
+            JsonNode json = play.libs.Json.toJson(recipe);
+            result = Results.ok(json)
                     .as(Http.MimeTypes.JSON);
+
         }else if(request().accepts(Http.MimeTypes.XML)){
-            result = ok("Retrieve XML"+recipeId)
+            Content content = views.xml.recipe.render(recipe);
+            result = Results.ok(content)
                     .as(Http.MimeTypes.XML);
+
         }else{
-            result = notAcceptable("Not Acceptable");
+            result = Results.notAcceptable("Not Acceptable");
         }
+
         return result;
     }
 
@@ -30,6 +47,38 @@ public class RecipeController extends Controller {
     }
 
     public Result listRecipes() {
-        return ok("List all recipes");
+        Result result;
+
+        Recipe recipe1 = new Recipe();
+        recipe1.setId(1);
+        recipe1.setName("Macarrones");
+        recipe1.setImageURL("imagen");
+        recipe1.setIngredients(new ArrayList<>());
+
+        Recipe recipe2 = new Recipe();
+        recipe2.setId(2);
+        recipe2.setName("Paella");
+        recipe2.setImageURL("imagen");
+        recipe2.setIngredients(new ArrayList<>());
+
+        List<Recipe> recipeList = new ArrayList<>();
+        recipeList.add(recipe1);
+        recipeList.add(recipe2);
+
+        if(request().accepts(Http.MimeTypes.JSON)){
+            JsonNode json = play.libs.Json.toJson(recipeList);
+            result = Results.ok(json)
+                    .as(Http.MimeTypes.JSON);
+
+        }else if(request().accepts(Http.MimeTypes.XML)){
+            Content content = views.xml.recipes.render(recipeList);
+            result = Results.ok(content)
+                    .as(Http.MimeTypes.XML);
+
+        }else{
+            result = Results.notAcceptable("Not Acceptable");
+        }
+
+        return result;
     }
 }
--- app/models/Recipe.java
+++ app/models/Recipe.java
@@ -2,9 +2,51 @@ package models;
 
 import java.util.ArrayList;
 
-public class Recipe{
-	private Integer id;
-	private String name;
-	private String imageURL;
-	private ArrayList<Ingredient> ingredients = new ArrayList<Ingredient>();
+public class Recipe {
+    private Integer id;
+    private String name;
+    private String imageURL;
+    private ArrayList<Ingredient> ingredients = new ArrayList<Ingredient>();
+
+    public Recipe() {
+    }
+
+    public Recipe(Integer id, String name, String imageURL, ArrayList<Ingredient> ingredients) {
+        this.id = id;
+        this.name = name;
+        this.imageURL = imageURL;
+        this.ingredients = ingredients;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    public ArrayList<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(ArrayList<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
 }
